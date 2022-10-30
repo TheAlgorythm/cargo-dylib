9822da1aff109f55bb9b92ebe60d8603723561bf
ZSchoen <dev@zschoen.dev>
2022-10-30 20:50:51 +0100

    added error handling + dynamic project root path

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,41 @@ use indoc::formatdoc;
 use rayon::prelude::*;
 use serde::Serialize;
 use std::fs;
+use std::path::Path;
 use std::process::{Command, Stdio};
+use tap::Tap;
 
-const DYNLIB_PATH: &str = "target/cargo-dylib/";
-const DYNLIB_MANIFEST_PATH: &str = "target/cargo-dylib/Cargo.toml";
+#[derive(thiserror::Error, Debug)]
+enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    TomlSerialize(#[from] toml::ser::Error),
+    #[error(transparent)]
+    CargoToml(#[from] cargo_toml::Error),
+    #[error(transparent)]
+    LocateManifest(#[from] locate_cargo_manifest::LocateManifestError),
+}
 
-fn main() {
+fn main() -> Result<(), Error> {
+    human_panic::setup_panic!();
+
     let Cargo::Dylib(cli) = Cargo::parse();
 
-    init_dylibs();
+    let real_manifest_path = locate_cargo_manifest::locate_manifest()?;
+    let dynlib_path = real_manifest_path
+        .clone()
+        .tap_mut(|real| {
+            real.pop();
+        })
+        .tap_mut(|root| root.extend(["target", "cargo-dylib"]));
+    let dynlib_manifest_path = dynlib_path.join("Cargo.toml");
 
-    invoke_cargo(&cli);
+    init_dylibs(&real_manifest_path, &dynlib_path, &dynlib_manifest_path)?;
+
+    invoke_cargo(&cli, &dynlib_manifest_path)?;
+
+    Ok(())
 }
 
 #[derive(Debug, Serialize)]
@@ -25,48 +49,48 @@ struct ManifestDependencies {
     pub dependencies: DepsSet,
 }
 
-fn init_dylibs() {
-    let real_manifest_path = "Cargo.toml";
-
-    let real_manifest_modified = fs::metadata(real_manifest_path)
-        .unwrap()
-        .modified()
-        .unwrap();
-    let dylib_manifest_modified = fs::metadata(DYNLIB_MANIFEST_PATH)
+fn init_dylibs(
+    real_manifest_path: &Path,
+    dynlib_path: &Path,
+    dynlib_manifest_path: &Path,
+) -> Result<(), Error> {
+    let real_manifest_modified = fs::metadata(real_manifest_path)?.modified()?;
+    let dylib_manifest_modified = fs::metadata(dynlib_manifest_path)
         .ok()
         .map(|metadata| metadata.modified())
-        .transpose()
-        .unwrap();
+        .transpose()?;
     if dylib_manifest_modified
         .map(|modified| real_manifest_modified < modified)
         .unwrap_or(false)
     {
-        return;
+        return Ok(());
     }
 
-    let real_manifest = Manifest::from_path(real_manifest_path).unwrap();
+    let real_manifest = Manifest::from_path(real_manifest_path)?;
     let mut dylib_manifest = real_manifest.clone();
 
-    fs::DirBuilder::new()
-        .recursive(true)
-        .create(DYNLIB_PATH)
-        .unwrap();
+    fs::DirBuilder::new().recursive(true).create(dynlib_path)?;
 
     dylib_manifest.dependencies = real_manifest
         .dependencies
         .par_iter()
-        .map(init_dep)
-        .collect();
+        .map(|dep| init_dep(dep, dynlib_path))
+        .collect::<Result<_, _>>()?;
 
     dylib_manifest.bin.first_mut().unwrap().path = Some("../../src/main.rs".to_string());
 
-    let dylib_manifest = toml::to_string(&dylib_manifest).unwrap();
-    std::fs::write(DYNLIB_MANIFEST_PATH, dylib_manifest).unwrap();
+    let dylib_manifest = toml::to_string(&dylib_manifest)?;
+    std::fs::write(dynlib_manifest_path, dylib_manifest)?;
+
+    Ok(())
 }
 
-fn init_dep(dep: (&String, &Dependency)) -> (String, Dependency) {
+fn init_dep(
+    dep: (&String, &Dependency),
+    dynlib_path: &Path,
+) -> Result<(String, Dependency), Error> {
     let dynamic_name = format!("{}-dynamic", dep.0);
-    let dynamic_crate_path = format!("{DYNLIB_PATH}{dynamic_name}");
+    let dynamic_crate_path = dynlib_path.join(&dynamic_name);
 
     let dep_detail = cargo_toml::DependencyDetail {
         path: Some(dynamic_name.clone()),
@@ -77,20 +101,20 @@ fn init_dep(dep: (&String, &Dependency)) -> (String, Dependency) {
     let dependency = (dep.0.clone(), Dependency::Detailed(dep_detail));
 
     if std::path::Path::new(&dynamic_crate_path).exists() {
-        return dependency;
+        return Ok(dependency);
     }
 
+    let dynamic_crate_src = dynamic_crate_path.join("src");
     fs::DirBuilder::new()
         .recursive(true)
-        .create(format!("{dynamic_crate_path}/src"))
-        .unwrap();
+        .create(&dynamic_crate_src)?;
 
     let mut dynamic_dependencies = DepsSet::new();
     dynamic_dependencies.insert(dep.0.clone(), dep.1.clone());
     let dynamic_dependencies = ManifestDependencies {
         dependencies: dynamic_dependencies,
     };
-    let dynamic_dependencies = toml::to_string(&dynamic_dependencies).unwrap();
+    let dynamic_dependencies = toml::to_string(&dynamic_dependencies)?;
 
     let dynamic_manifest = formatdoc!(
         "
@@ -106,28 +130,27 @@ fn init_dep(dep: (&String, &Dependency)) -> (String, Dependency) {
         "
     );
 
-    std::fs::write(format!("{dynamic_crate_path}/Cargo.toml"), dynamic_manifest).unwrap();
+    std::fs::write(dynamic_crate_path.join("Cargo.toml"), dynamic_manifest)?;
 
     std::fs::write(
-        format!("{dynamic_crate_path}/src/lib.rs"),
+        dynamic_crate_src.join("lib.rs"),
         format!("pub use {}::*;", dep.0),
-    )
-    .unwrap();
+    )?;
 
-    dependency
+    Ok(dependency)
 }
 
-fn invoke_cargo(cli: &DylibCli) {
+fn invoke_cargo(cli: &DylibCli, dynlib_manifest_path: &Path) -> std::io::Result<()> {
     Command::new("cargo")
         .arg(&cli.subcommand)
         .arg("--manifest-path")
-        .arg(DYNLIB_MANIFEST_PATH)
+        .arg(dynlib_manifest_path)
         .args(&cli.arguments)
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
+        .spawn()?
+        .wait()?;
+
+    Ok(())
 }
